f182ff4b494d0caba373ae9519157029def9aed1
Eduardo Loza <spcr_07@yahoo.com>
2017-07-07 01:59:49 -0500

    Combined init and scan in one python script

--- Python Codes/RPLidar.py
+++ Python Codes/RPLidar.py
@@ -15,51 +15,49 @@ class InitProcess(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
-    def run(self, dumpFile, dumpLoc): # send dump, not data
+    def run(self): # send dump, not data
+        global dumpFile, dumpLoc, dataFile, dataLoc
+        global initProcess
         if not(dumpFile.closed):
-            dumpFile.close() # Release from previous one
-        dumpFile = open(fileLoc, "a")
+            dumpFile.close()
+            dumpFile = open(dumpLoc, "a")
+        else:
+            dumpFile = open(dumpLoc, "a")
         # Should start writing to dump file
-        dumpFile.write("InitProcess: Initiating ROS")
         # Starts ROS initialization
-        process = subprocess.Popen('./runpy-1.sh', stdout = subprocess.PIPE)
-        for line in stdout:
-            dumpFile.write("InitProcess: " + str(line))
-        dumpFile.close() # This might not work since shell script will keep run
-
+        initProcess = subprocess.Popen('./runpy-1.sh', stdout = subprocess.PIPE)
+        for line in initProcess.stdout:
+            dumpFile.write(str(datetime.now()) + " Init Process: " + str(line) + "\n")
+        
 class ScanProcess(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
 
-    def run(self, dumpFile, dataFile, dumpLoc, dataLoc):
-        global mainWorking
+    def run(self):
+        global dumpFile, dumpLoc, dataFile, dataLoc
+        global scanProcess
         while True:
-            if not(dumpFile.closed):
-                dumpFile.close()
-                dumpFile = open(dumpLoc, "a")
-            if not(dataFile.close):
-                dataFile.close()
-                dataFile = open(dataLoc, "a")
-
-           getData()
-           writeData()
-
-class RPLidarData(object):
-
-    timestamp = "inf"
-    lidarAngle = "inf"
-    lidarDist = "inf"
-    lidarOutput = "inf"
-
-    def output(time, angle, dist):
-        lidarOutput = "," + time + " = " + timestamp + "," + angle + " = " + lidarAngle + "," + dist + " = " + lidarDist
-        
+            getData()
+            writeData()
                 
-def getData(channel = 0, decimalPlace = 6):
-    global process
+def getData(channel = 0, decimalPlaces = 6):
+    global dumpLoc, dumpFile, dataLoc, dataFile
+    global scanProcess
     global isValid
-    outputline = process.stdout.readline()
+    
+    if not(dumpFile.closed):
+        dumpFile.close()
+        dumpFile = open(dumpLoc, "a")
+    else:
+        dumpFile = open(dumpLoc, "a")
+    if not(dataFile.close):
+        dataFile.close()
+        dataFile = open(dataLoc, "a")
+    else:
+        dataFile = open(dataLoc, "a")
+                    
+    outputline = scanProcess.stdout.readline()
     parsed = str(outputline, 'utf-8')
     splits = parsed.split(": : ")
     if len(splits) > 1:
@@ -84,33 +82,71 @@ def getData(channel = 0, decimalPlace = 6):
         lidarOutput = ",TIME = " + timestamp + ",ANGL = " + lidarAngle + ",DIST = " + lidarDist
         isValid = True
     else:
-        global dumpFile
-        dumpFile.write("Malformed Data: ", parsed)
+        dumpFile.write(str(datetime.now()) + " Malformed Data: " + parsed + "\n")
 
 def writeData():
+    global dumpLoc, dumpFile, dataLoc, dataFile
     global lidarOutput
     global isValid
+
+    if not(dumpFile.closed):
+        dumpFile.close()
+        dumpFile = open(dumpLoc, "a")
+    else:
+        dumpFile = open(dumpLoc, "a")
+    if not(dataFile.close):
+        dataFile.close()
+        dataFile = open(dataLoc, "a")
+    else:
+        dataFile = open(dataLoc, "a")
+            
     if isValid:
         now = datetime.now()
-        file.write(str(now) + "," + str(angle) + lidarOutput + "\n")
-        file.flush()
+        dataFile.write(str(now) + "," + str(angle) + lidarOutput + "\n")
+        dataFile.flush()
         isValid = False
 
 # Variables
+initProcess = None
+scanProcess = None
+
 mainWorking = True # For thread handling
 isValid = False # For data validity
 
+# ADC SPI
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
 angle = 0 # Potentiometer angle report
 
+# RPLidar Data
+timestamp = "inf" #timestamp for Lidar
+lidarAngle = "inf" # Lidar angle
+lidarDist = "inf" # Lidar distance
+lidarOutput = "inf" # Used to combine all
+
+initProc = InitProcess() # InitProc thread will initialize RPLidar to use
+scanProc = ScanProcess() # ScanProc thread will handle data from potentiometer and LIDAR
+
 fileCount = 0
-fileName = "/home/edward/Desktop/LIDARDUMP/LIDAR_data" + str(fileCount) + ".csv"
-
-while(os.path.isfile(fileName)):
-    fileCount += 1 ;
-    fileName = "/home/edward/Desktop/LIDARDUMP/LIDAR_data" + str(fileCount) + ".csv"
-
-dumpLoc = "/home/edward/Desktop/LIDARDUMP/LIDAR_data" + str(fileCount) + "_dump.txt"
+dumpLoc = "/home/edward/Desktop/LIDARDUMP/LIDAR_dump" + str(fileCount) + ".txt"
 dataLoc = "/home/edward/Desktop/LIDARDUMP/LIDAR_data" + str(fileCount) + ".csv"
+
+# Files first
+while(os.path.isfile(dataLoc)):
+    fileCount += 1
+    dumpLoc = "/home/edward/Desktop/LIDARDUMP/LIDAR_dump" + str(fileCount) + ".txt"
+    dataLoc = "/home/edward/Desktop/LIDARDUMP/LIDAR_data" + str(fileCount) + ".csv"
+
+dumpFile = open(dumpLoc, "w") # Used to dump EVERYTHING
+dataFile = open(dataLoc, "w") # Used to save all proper data
+
+scanProcess = subprocess.Popen('./runpy-2.sh', bufsize = -1, stdout = subprocess.PIPE)
+    
+# Start initProc first
+initProc.start()
+scanProc.start()
+
+# Start reading
+initProc.join()
+scanProc.join()
