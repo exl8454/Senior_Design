f940b2b6a3d0baebc18e1312ca0eef0e086b251e
Charlie S. Aka CS <hsmchul@hotmail.com>
2017-07-08 21:18:01 -0500

    Renamed Folder to Avocado

--- Python Codes/Avocado/runpy-1.sh
+++ Python Codes/Avocado/runpy-1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd ~/sd
-ls /dev/ttyUSB0
 echo -e "Red!23\n" | sudo -S chmod 666 /dev/ttyUSB0
 source devel/setup.bash
 roslaunch rplidar_ros rplidar.launch & rosrun rplidar_ros rplidarNodeClient
