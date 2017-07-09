26fbacc78b271ef520698ab248ac97dbd1a5a9d6
Eduardo Loza <spcr_07@yahoo.com>
2017-07-08 20:39:56 -0500

    Updated Avocado

--- Python Codes/Core/runpy-1.sh
+++ Python Codes/Core/runpy-1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~/sd
-echo -e "Red!23\n" | sudo -S chmod 666 /dev/ttyUSB*
+ls /dev/ttyUSB0
+echo -e "Red!23\n" | sudo -S chmod 666 /dev/ttyUSB0
 source devel/setup.bash
-roslaunch rplidar_ros rplidar.launch
+roslaunch rplidar_ros rplidar.launch & rosrun rplidar_ros rplidarNodeClient
