f0516199a0d9028261e761b24c0fda35f7d1b9f5
Charlie S. Aka CS <hsmchul@hotmail.com>
2017-07-09 19:37:50 -0500

    Updated Front-End

--- Python Codes/Avocado/Avocado.py
+++ Python Codes/Avocado/Avocado.py
@@ -15,6 +15,16 @@ avocado_config = []
 avocado_filedir = "LIDARDUMP"
 #avocado_filedir = "/home/edward/Desktop/LIDARDUMP"
 
+def CommandControl():
+    print ("Type exit to stop process...")
+    command = ""
+    while (command != "exit"):
+        command = input("AVOCADO>>>")
+
+    stream.PrintTo("Suspending Avocado...", "INFO")
+    roscore.TerminateCore()
+    stream.PrintTo("Goodby!", "INFO")
+
 stream.PrintTo("Avocado 1.0", "INFO")
 stream.PrintTo("Loading configuration file...", "INFO")
 
@@ -33,13 +43,3 @@ stream.PrintTo("Starting ROS Service...", "INFO")
 
 _thread.start_new_thread(CommandControl(), ())
 roscore.StartCore()
-
-def CommandControl():
-    print ("Type exit to stop process...")
-    command = ""
-    while (command != "exit"):
-        command = input("AVOCADO>>>")
-
-    stream.PrintTo("Suspending Avocado...", "INFO")
-    roscore.TerminateCore()
-    stream.PrintTo("Goodby!", "INFO")
