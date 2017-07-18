c4bddb61088622cee4514ebecdd025887e005bcb
Charlie S. Aka CS <hsmchul@hotmail.com>
2017-07-17 21:02:12 -0500

    Created Importable Avocado Visualizer

---
+++ Avocado_Native/avcvis.py
@@ -0,0 +1,17 @@
+# avcvis.py
+# AvocadoVis importable script
+
+# Native Import
+import time
+import math
+
+# Third-Part Import
+import pygame
+
+# Avocado Import
+from LidarHandler import LidarHanlder as Lidar
+
+class AvocadoVis(object):
+
+    def __init__():
+        return
