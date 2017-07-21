b0cb2d86cb269df77a423aaae8e9a932303be81b
Charlie S. Aka CS <hsmchul@hotmail.com>
2017-07-20 22:49:20 -0500

    Edited Transmission Sequence

--- Avocado_Native/ServoHandler.py
+++ Avocado_Native/ServoHandler.py
@@ -5,6 +5,7 @@
 
 # Native import
 import threading
+import time
 
 # Thrid-party import
 import serial
@@ -24,41 +25,30 @@ class ArduProcess(threading.Thread):
 # Attempts to start arduino on target port.
 # If started, arduino should return start code.
 # Otherwise, arduino will return -1
-def StartServo(target_port, time_out):
-    arduino = serial.Serial(target_port, 115200, timeout = time_out)
-    arduino.write("avc start\n")
-    while not(arduino.in_waiting >= 4):
+def StartServo(target_port):
+    global arduino
+    arduino = serial.Serial(port = target_port, baudrate = 115200)
+    time.sleep(3)
+    arduino.write(b'avc_start\r')
+    while (arduino.in_waiting <= 4):
         pass
-    code = arduino.read()
-    code = str(code.decode('utf-8'))
-    if (code in ['ack']):
+    code = arduino.readline()
+    code = str(code.decode('ascii'))
+    if (code in ['ack\r\n']):
         return 0
     else:
         return -1
 
-# Sends new delay between servo turn.
-# Funtion will attempt to receive new delay from arduino then return the value.
-def SetServoSpeed(delay_in_milli):
-    if not(arduino is None):
-        arduino.write("avc del " + str(delay_in_milli) + "\n")
-        while not(arduino.in_waiting >= 4):
-            pass
-        value = arduino.read()
-        if(code in [str(delay_in_milli)]):
-           return value
-    else:
-        stream.PrintTo("No arduino detected", "ERR")
-        return -1
-
-# Sends signal to Arduino to force stop servo
+# Sends signal to Arduino to move servo angle to center (90 degrees)
 def StopServo():
+    global arduino
     if not(arduino is None):
-        arduino.write("avc stp\n")
-        while not(arduino.in_waiting >= 3):
+        arduino.write(b'avc stp\r')
+        while (arduino.in_waiting <= 4):
             pass
-        code = arduino.read()
-        code = str(code.decode('utf-8'))
-        if (code in ['ack']):
+        code = arduino.readline()
+        code = str(code.decode('ascii'))
+        if (code in ['ack\r\n']):
             return 0
         else:
             return -1
@@ -66,14 +56,31 @@ def StopServo():
         stream.PrintTo("No arduino detected", "ERR")
         return -1
 
+# Sends new delay between servo turn.
+# Funtion will attempt to receive new delay from arduino then return the value.
+def SetServoSpeed(delay_in_milli):
+    global arduino
+    if not(arduino is None):
+        cmd = "avc del " + str(delay_in_milli) + "\r"
+        arduino.write(cmd.encode(encoding = 'ascii'))
+        while not(arduino.in_waiting >= 4):
+            pass
+        value = int(arduino.readline())
+        if value == delay_in_milli:
+           return value
+    else:
+        stream.PrintTo("No arduino detected", "ERR")
+        return -1
+
 # Requests servo's current angle then saves to a variable
 def GetServoAngle():
+    global arduino
     if not(arduino is None):
-        arduino.write("avc get agl\n")
+        arduino.write(b'avc get agl\r')
         while not(arduino.in_waiting >= 3):
             pass
-        code = arduino.read()
-        code = str(code.decode('utf-8'))
+        code = arduino.readline()
+        code = str(code.decode('ascii'))
         global servo_angle
         servo_angle = int(code)
 
